bd78ccaf9ad53eee8aba4e11b93bbde172681b20
Rakibulislam1 <rirakib03@gmail.com>
2023-10-19 16:23:43 +0600

    error page and error gif added

--- src/Component/Error/Error.jsx
+++ src/Component/Error/Error.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+import gif from "../../assets/404.gif";
 
 const Error = () => {
   return (
     <div>
-      
+      <div className="h-[70vh] flex justify-center items-center">
+        <img className="w-96" src={gif} alt="" />
+      </div>
+      <Link to='/'><button className="btn btn-accent block mx-auto">Go Back Home</button></Link>
     </div>
   );
 };
